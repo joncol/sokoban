a9d7bbdd594de85e30a2c69bd4030aed4a8f764c
Jonas Collberg <jonas.collberg@gmail.com>
2018-05-12 17:40:39 +0200

    Keep track of player position and movable block positions

--- src/cljs/sokoban/db.cljs
+++ src/cljs/sokoban/db.cljs
@@ -1,7 +1,16 @@
 (ns sokoban.db
   (:require [clojure.string :as str]
-            [sokoban.slurp :include-macros true :refer [slurp]]))
+            [sokoban.slurp :include-macros true :refer [slurp]]
+            [sokoban.util :refer [find-value]]))
+
+(defn block-positions [level block]
+  (->> (for [y (find-value #(str/includes? % block) level)]
+         (map #(-> [y %]) (find-value #(= % block) (get level y))))
+       (apply concat)))
 
 (def default-db
-  {:name "sokoban"
-   :level (str/split (slurp "resources/level01.txt") #"\n")})
+  (let [level (str/split (slurp "resources/level01.txt") #"\n")]
+    {:name           "sokoban"
+     :static-level   (mapv #(vec (str/replace % #"[@$]" " ")) level)
+     :player-pos     (first (block-positions level "@"))
+     :movable-blocks (block-positions level "$")}))
--- src/cljs/sokoban/subs.cljs
+++ src/cljs/sokoban/subs.cljs
@@ -7,6 +7,29 @@
     (:active-panel db)))
 
 (rf/reg-sub
-  ::level
+  ::static-level
   (fn [db]
-    (:level db)))
+    (:static-level db)))
+
+(rf/reg-sub
+  ::player-pos
+  (fn [db]
+    (:player-pos db)))
+
+(rf/reg-sub
+  ::movable-blocks
+  (fn [db]
+    (:movable-blocks db)))
+
+(rf/reg-sub
+  ::level
+  (fn [_]
+    [(rf/subscribe [::static-level])
+     (rf/subscribe [::player-pos])
+     (rf/subscribe [::movable-blocks])])
+  (fn [[static-level player-pos movable-blocks]]
+    (-> static-level
+        (assoc-in player-pos "@")
+        (as-> l
+            (reduce #(assoc-in %1 %2 "$") l movable-blocks))
+        (assoc-in player-pos "@"))))
