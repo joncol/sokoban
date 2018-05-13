90c40f83cc419c834f21c304f66db12d1f09d0f2
Jonas Collberg <jonas.collberg@gmail.com>
2018-05-13 10:30:55 +0200

    Pad game board

--- src/cljs/sokoban/db.cljs
+++ src/cljs/sokoban/db.cljs
@@ -3,9 +3,17 @@
             [sokoban.slurp :include-macros true :refer [slurp]]
             [sokoban.game-util :refer [block-positions]]))
 
+(defn- pad-vec [v width]
+  (let [n (count (take-while #(= " " %) v))]
+    (vec (concat (repeat n "#") (drop n v) (repeat (- width (count v)) "#")))))
+
 (def default-db
-  (let [level (str/split (slurp "resources/level01.txt") #"\n")]
-    {:static-level            (mapv #(vec (str/replace % #"[@$]" " ")) level)
+  (let [level (str/split (slurp "resources/level01.txt") #"\n")
+        width (count (apply max-key count level))]
+    {:static-level            (mapv (fn [row]
+                                      (pad-vec (str/replace row #"[@$]" " ")
+                                               width))
+                                    level)
      :target-positions        (block-positions level ".")
      :player-position-history [(first (block-positions level "@"))]
      :movable-blocks-history  [(block-positions level "$")]
