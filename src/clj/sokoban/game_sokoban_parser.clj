6382661cd7043843dbd22cfa95f57640b1c0d362
Jonas Collberg <jonas.collberg@gmail.com>
2018-05-15 21:49:51 +0200

    Fix parsing functions and add single catalog parsing

--- src/clj/sokoban/game_sokoban_parser.clj
+++ src/clj/sokoban/game_sokoban_parser.clj
@@ -4,7 +4,10 @@
             [hickory.core :as h]
             [hickory.select :as s]))
 
-(defn- parse-catalog-link [link]
+(defn- parse-catalog-link
+  "Parses a link to a catalog. Returns a map with keys `:id`, `:name`, `:size`,
+  and `:url`."
+  [link]
   (let [href    (->> link :attrs :href)
         cid     (Integer/parseInt (second (re-find #"cid=(\d+)" href)))
         content (-> link :content)
@@ -19,29 +22,80 @@
      :size size
      :url  (str "http://www.game-sokoban.com" href)}))
 
-(defn extract-catalogs [body]
-  (let [body-htree (-> body h/parse h/as-hickory)
-        links      (-> (s/select (s/descendant
-                                  (s/class :lblock)
-                                  (s/and (s/tag :a)
-                                         (s/attr :href)
-                                         (s/class :collection-link)))
-                                 body-htree))]
-    (map parse-catalog-link links)))
+(defn extract-catalog-list
+  "Parses the main page and returns a vector of all the catalogs."
+  [body]
+  (let [htree (-> body h/parse h/as-hickory)
+        links (-> (s/select (s/descendant
+                             (s/class :lblock)
+                             (s/and (s/tag :a)
+                                    (s/attr :href)
+                                    (s/class :collection-link)))
+                            htree))]
+    (mapv parse-catalog-link links)))
 
-(defn extract-catalog [body]
-  )
+(defn- parse-level-link
+  "Parses a link to a level (class: `catalog-item1`). Returns a map with keys
+  `:id` and `:name`."
+  [link]
+  (let [name (-> (s/select (s/child (s/and
+                                     (s/tag :div)
+                                     (s/class :top1)))
+                           link)
+                 first :content first)
+        href (-> (s/select (s/child (s/and
+                                     (s/tag :a)
+                                     (s/attr :href)))
+                           link)
+                 first :attrs :href)
+        lid  (Integer/parseInt (second (re-find #"lid=(\d+)" href)))]
+    {:id   lid
+     :name name}))
 
-(defn extract-level [body]
-  (let [body-htree (-> body h/parse h/as-hickory)
-        code       (-> (s/select (s/child (s/has-child
-                                           (s/and
-                                            (s/tag :div)
-                                            (s/class :header)
-                                            (s/find-in-text #"Code:")))
-                                          s/last-child
-                                          (s/tag :pre))
-                                 body-htree)
-                       first :content first)]
+(defn extract-catalog-page-count
+  "Parses a catalog page for the number of pages."
+  [body]
+  (let [htree (-> body h/parse h/as-hickory)
+        pager (-> (s/select (s/child
+                             (s/class :rblock)
+                             (s/class :main-block)
+                             (s/class :pager))
+                            htree)
+                  first)]
+    (-> (s/select (s/child
+                   (s/and
+                    (s/tag :a)
+                    (s/attr :href)))
+                  pager)
+        count
+        inc ;; Since current page is not counted.
+        )))
+
+(defn extract-catalog
+  "Parses a catalog page for all level links. Returns a vector of maps, where
+  each map has keys `:id` and `:name`."
+  [body]
+  (let [htree (-> body h/parse h/as-hickory)
+        links (-> (s/select (s/child
+                             (s/class :rblock)
+                             (s/class :main-block)
+                             (s/class :catalog-item1))
+                            htree))]
+    (mapv parse-level-link links)))
+
+(defn extract-level
+  "Parses a level page and returns a vector of strings, where each string is a
+  row of the level."
+  [body]
+  (let [htree (-> body h/parse h/as-hickory)
+        code  (-> (s/select (s/child (s/has-child
+                                      (s/and
+                                       (s/tag :div)
+                                       (s/class :header)
+                                       (s/find-in-text #"Code:")))
+                                     s/last-child
+                                     (s/tag :pre))
+                            htree)
+                  first :content first)]
     (->> (str/split code #"\n")
-         (map str/trimr))))
+         (mapv str/trimr))))
--- src/clj/sokoban/handler.clj
+++ src/clj/sokoban/handler.clj
@@ -6,11 +6,15 @@
             [hiccup.page :refer [include-js include-css html5]]
             [org.httpkit.client :as http-client]
             [ring.util.response :as resp]
-            [sokoban.game-sokoban-parser :refer [extract-catalogs
-                                                 extract-level]]
-            [sokoban.middleware :refer [wrap-middleware]]))
+            [sokoban.game-sokoban-parser :refer [extract-catalog-list
+                                                 extract-catalog
+                                                 extract-level
+                                                 extract-catalog-page-count]]
+            [sokoban.middleware :refer [wrap-middleware]]
+            [sokoban.util :refer [find-value]]))
 
 (def catalog-list-cache (atom []))
+(def catalog-cache (atom {}))
 (def level-cache (atom {}))
 
 (def mount-target
@@ -43,10 +47,36 @@
           (do (log/error (str "Failed to download catalogs from "
                               "http://www.game-sokoban.com: " (:error resp)))
               (resp/response resp))
-          (let [catalogs (-> resp :body extract-catalogs)]
+          (let [catalogs (-> resp :body extract-catalog-list)]
             (log/debug "Downloaded catalogs from http://www.game-sokoban.com")
             (reset! catalog-list-cache catalogs)
             (resp/response catalogs))))))
+  (GET "/catalog/:id" [id]
+    (if (contains? @catalog-cache id)
+      (do
+        (log/debug "Returning cached catalog:" id)
+        (resp/response (@catalog-cache id)))
+      (let [page-size  24
+            page-count (-> @(http-client/get
+                             (str "http://www.game-sokoban.com/index.php"
+                                  "?mode=catalog")
+                             {:query-params {:cid  id
+                                             :page 1
+                                             :q    page-size}})
+                           :body
+                           extract-catalog-page-count)]
+        (-> (reduce (fn [levels page]
+                      (concat levels
+                              (-> @(http-client/get
+                                    (str "http://www.game-sokoban.com/index.php"
+                                         "?mode=catalog")
+                                    {:query-params {:cid  id
+                                                    :page (inc page)
+                                                    :q    page-size}})
+                                  :body
+                                  extract-catalog)))
+                    [] (range page-count))
+            resp/response))))
   (GET "/level/:id" [id]
     (if (contains? @level-cache id)
       (do
